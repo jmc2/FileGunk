9c91425c48567325e1784dd239e0f845bf00e3b1
Joseph Curcuru <stoneinfocus@gmail.com>
2011-09-03 18:15:30 -0400

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileGunk::Application.config.secret_token = 'e44b1be3a2acc8c8b0f06a76c0e6d09b5ddfd0099f3ef414374acafcade8383d96a6eca7b6dc545f9d952dba0c389d3be3315d77d67b0f6080de9debfe68b702'
